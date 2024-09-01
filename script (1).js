139228d377728376a639a4437bdcffaaf7559103
caiobreno2008 <caiobreno155@gmail.com>
2024-09-01 20:08:31 -0300

    Update script (1).js
    nova forma star tio everson

--- script (1).js
+++ script (1).js
@@ -1,7 +1,7 @@
-const vehicle1 = ('Dê um nome para um veículo.')
-const vehicle2 = ('Dê outro nome para outro veículo.')
-const speed1 = parseFloat(prompt(`Determine a velocidade de ${vehicle1}`));
-const speed2 = parseFloat(prompt(`Determine a velocidade de ${vehicle2}`));
+const vehicle1 = 'Carro A'; // Nome do primeiro veículo
+const vehicle2 = 'Carro B'; // Nome do segundo veículo
+const speed1 = 120; // Velocidade do primeiro veículo (em km/h)
+const speed2 = 100; // Velocidade do segundo veículo (em km/h)
 
 if (speed1 > speed2) {
     console.log(`${vehicle1} está a ${speed1} km/h, e é mais rápido que o ${vehicle2}, que está a ${speed2} km/h.`);
