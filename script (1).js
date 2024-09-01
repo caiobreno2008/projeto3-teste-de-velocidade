136de2fe38764f2e8d91fbb77011be00a44650aa
caiobreno2008 <caiobreno155@gmail.com>
2024-09-01 20:00:56 -0300

    Update script (1).js
    nova forma

--- script (1).js
+++ script (1).js
@@ -1,13 +1,12 @@
-let vehicle1 = prompt('Dê um nome para um veículo.')
-let vehicle2 = prompt('Dê outro nome para outro veículo.')
-const speed1 = prompt(`Determine a velocidade de ${vehicle1}`)
-const speed2 = prompt(`Determine a velocidade de ${vehicle2}`)
-
+const vehicle1 = ('Dê um nome para um veículo.')
+const vehicle2 = ('Dê outro nome para outro veículo.')
+const speed1 = parseFloat(prompt(`Determine a velocidade de ${vehicle1}`));
+const speed2 = parseFloat(prompt(`Determine a velocidade de ${vehicle2}`));
 
 if (speed1 > speed2) {
-    alert(`${vehicle1} está a ${speed1}km/h, e mais rápido que o ${vehicle2}, que está a ${speed2}km/h.`)
+    console.log(`${vehicle1} está a ${speed1} km/h, e é mais rápido que o ${vehicle2}, que está a ${speed2} km/h.`);
 } else if (speed1 < speed2) {
-    alert(`${vehicle2} está a ${speed2}km/h, e mais rápido que o ${vehicle1}, que está a ${speed1}km/h`)
+    console.log(`${vehicle2} está a ${speed2} km/h, e é mais rápido que o ${vehicle1}, que está a ${speed1} km/h.`);
 } else {
-    alert(`Tanto ${vehicle1} e ${vehicle2} estão a ${speed1 || speed2} km/h`)
+    console.log(`Tanto ${vehicle1} quanto ${vehicle2} estão a ${speed1} km/h.`);
 }
